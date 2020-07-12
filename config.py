a6dba3b774e4d3fc5fffd30595e3c2a5b1ac3629
amazeryogo <suvid.datta@icloud.com>
2020-07-12 11:11:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import psycopg2
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
@@ -10,7 +11,7 @@ class Config(object):
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               #'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    DATABASE_URL = 'postgres://jyyaacgvnxyvdv:bc8c5803c3b2fe88ca311afa2fbc48f27b369b07b67f04fd5dfcccd10758c61e@ec2-34-206-31-217.compute-1.amazonaws.com:5432/d662ja5jkhsdhu'
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
