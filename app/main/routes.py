c5a4b69f7b3b56101c4f5845aa14ebb8caf4b721
Amazeryogo <suvid.datta@gmail.com>
2020-11-20 14:34:47 +0530

    jjj

--- app/main/routes.py
+++ app/main/routes.py
@@ -35,7 +35,6 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
-        return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
