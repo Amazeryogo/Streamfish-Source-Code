f52ffd052ceb557d9869ea045154e2221ff9ed74
Amazeryogo <suvid.datta@gmail.com>
2020-12-04 09:40:33 +0530

    ||

--- app/main/forms.py
+++ app/main/forms.py
@@ -45,5 +45,5 @@ class SearchForm(FlaskForm):
 
 class MessageForm(FlaskForm):
     message = TextAreaField(_l('Message'), validators=[
-        DataRequired(), Length(min=1, max=140)])
+        DataRequired(), Length(min=1, max=10000000)])
     submit = SubmitField(_l('Post it'))
