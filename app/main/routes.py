7f108ce65ecbf95ff648f1fb6f328845618863f4
Suvid Datta <suvid.datta@gmail.com>
2020-12-09 18:29:10 +0530

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -158,6 +158,7 @@ def translate_text():
 @bp.route('/search')
 @login_required
 def search():
+    es = Elasticsearch(http_auth=('suvid', 'yoitsme'))
     if not g.search_form.validate():
         return redirect(url_for('main.explore'))
     page = request.args.get('page', 1, type=int)
