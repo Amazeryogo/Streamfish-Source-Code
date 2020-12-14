d352e8413bda77e59e1d140337ea9b78f18a8a12
Amazeryogo <suvid.datta@gmail.com>
2020-12-14 14:47:51 +0530

    v

--- app/api/auth.py
+++ app/api/auth.py
@@ -5,7 +5,6 @@ from app.api.errors import error_response
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
 
-
 @basic_auth.verify_password
 def verify_password(username, password):
     user = User.query.filter_by(username=username).first()
