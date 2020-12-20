dd39224e67c69bf1762e1233d26234edeaa0a262
Amazeryogo <suvid.datta@gmail.com>
2020-12-20 15:10:09 +0530

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,7 +10,6 @@ from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm, \
 from app.models import User, Post, Message, Notification
 from app.translate import translate
 from app.main import bp
-from elasticsearch1 import Elasticsearch
 
 
 @bp.before_app_request
