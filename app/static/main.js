d07081aa050806bf30665304d5fdd03165ac91af
Amazeryogo <suvid.datta@gmail.com>
2021-01-13 23:02:27 +0530

    Update main.js

--- app/static/main.js
+++ app/static/main.js
@@ -1,12 +1,4 @@
 function darkMode(){
  var element = document.body;
   element.classList.toggle("dark-mode");
-}
-
-service firebase.storage {
-  match /b/{bucket}/o {
-    match /{allPaths=**} {
-      allow read, write: if request.auth != null;
-    }
-  }
-} 
+}   
