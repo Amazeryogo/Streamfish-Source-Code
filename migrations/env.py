095e28e324feb658c4908a4c53612cf1c49e8a9a
Amazeryogo <suvid.datta@gmail.com>
2021-01-17 18:11:54 +0530

    Removed random stuff

--- migrations/env.py
+++ migrations/env.py
@@ -13,10 +13,6 @@ config = context.config
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 from flask import current_app
 config.set_main_option('sqlalchemy.url',
                        current_app.config.get('SQLALCHEMY_DATABASE_URI'))
