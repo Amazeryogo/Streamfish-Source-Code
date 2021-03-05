81723ca639bd3102d3fa09eca082ac7c13bb3569
Suvid Datta <suvid.datta@gmail.com>
2021-03-05 14:56:20 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -256,7 +256,7 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    body = db.Column(db.String(140))
+    body = db.Column(db.String(500))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
