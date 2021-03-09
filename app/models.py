227cca524f4a28534ff75018f23bdfd04ed13503
Suvid <suvid.datta@gmail.com>
2021-03-09 14:57:18 +0530

    more longpost stuff

--- app/models.py
+++ app/models.py
@@ -270,7 +270,9 @@ class LP(db.Model):
     body = db.Column(db.String(5000))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    
+    def __repr__(self):
+        return '<LP {}>'.format(self.title)
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True)
