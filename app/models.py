24737d76a7c7e65b9d55c866142a1965019ceebd
Amazeryogo <suvid.datta@gmail.com>
2021-04-25 13:36:01 +0530

    migrations

--- app/models.py
+++ app/models.py
@@ -281,6 +281,8 @@ class LP(db.Model):
 
     def __repr__(self):
         return '<LP {}>'.format(self.title)
+
+
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True)
