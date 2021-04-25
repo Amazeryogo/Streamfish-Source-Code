06a0f91761a7b4bc96bdce028335b30bbaac65f3
Amazeryogo <suvid.datta@gmail.com>
2021-04-25 13:49:46 +0530

    lmao

--- app/models.py
+++ app/models.py
@@ -252,7 +252,7 @@ def load_user(id):
 class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    body = db.Column(db.String(500))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     language = db.Column(db.String(5))
