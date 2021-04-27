8b5f38facd9824a64fdf514fa3df13a058c972e0
Amazeryogo <suvid.datta@gmail.com>
2021-04-27 13:30:28 +0530

    :

--- app/main/routes.py
+++ app/main/routes.py
@@ -125,8 +125,6 @@ def user(username):
         return render_template('Hindi/user.html',user=user, posts=posts.items,
                             next_url=next_url, prev_url=prev_url, form=form,verified=verified,staff=staff,dm=current_user.darkmode)
 
-
-
 @bp.route('/user/<username>/popup')
 @login_required
 def user_popup(username):
